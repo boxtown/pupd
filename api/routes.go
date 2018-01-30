2e32c139dc26671c86dbfdb946ba64a0b002e93c
Michael Ma <michael60612@gmail.com>
2018-01-29 21:35:23 -0500

    feat: CORS for api

--- api/routes.go
+++ api/routes.go
@@ -8,12 +8,17 @@ import (
 	"github.com/boxtown/pupd/model"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 // Router returns a router http.Handler
 // to be served by `http.ListenAndServe`
 func Router(source *pg.DataSource, stores model.Stores) http.Handler {
 	r := chi.NewRouter()
+
+	cors := cors.Default()
+	r.Use(cors.Handler)
+
 	r.Route("/movements", func(r chi.Router) {
 		r.Get("/", listMovementsFn(source, stores))
 		r.Post("/", createMovementFn(source, stores))
